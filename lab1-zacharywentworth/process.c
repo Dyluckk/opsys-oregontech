d5f29147787ce5777315394ad6f520fbafe92a63
zachary.wentworth <Dyluckk>
2017-04-12 18:17:28 -0700

    initial lab 1 commit

---
+++ lab1-zacharywentworth/process.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> /* for fork */
+#include <sys/types.h> /* for pid_t */
+#include <sys/wait.h> /* for wait */
+
+int main (int argc, char *argv[])
+{
+    printf("hello world (pid:%d)\n", (int) getpid());
+    int rc = fork();
+    if (rc < 0) { // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("hello, I am child (pid:%d)\n", (int) getpid());
+    } else { // parent goes down this path (main)
+        printf("hello, I am parent of %d (pid:%d)\n", rc, (int) getpid());
+    }    
+    return 0;
+}
